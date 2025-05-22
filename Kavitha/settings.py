89ab1dd207b879867717b50f8f103f9a37e4d9fb
kavitha <kavithathangan006@gmail.com>
2025-05-22 16:02:36 +0530

    day7

--- Kavitha/settings.py
+++ Kavitha/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'Kavitha.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':{
+       'ENGINE':'django.db.backends.mysql',
+       'NAME':'yellow',
+       'USER':'root',
+       'PASSWORD':'kavitha',
+       'HOST':'localhost',
+       'PORT':'3307',
+   } 
 }
 
 
--- digital/views.py
+++ digital/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 def homepage(request):
     return render(request,'index.html')
+
+def student_form(request):
+    if request.method =='POST':
+        name=request.POST.get('name')
+        rollno=request.POST.get('rollno')
+        email=request.POST.get('email')
+        student.objects.create(name=name,rollno=rollno,email=email)
+        return redirect('student_form')
+    students=student.objects.all()
+    return render(request,'index.html',{'students':students})
+
+
+
+
